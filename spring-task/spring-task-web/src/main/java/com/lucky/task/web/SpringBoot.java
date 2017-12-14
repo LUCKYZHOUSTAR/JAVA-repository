37e6f6d256f92645f3ffb797c519deb13a27b88b
周超强 <chaoqiang.zhou@Mtime.com>
2017-12-14 17:54:54 +0800

    add quartz and jetty

---
+++ spring-task/spring-task-web/src/main/java/com/lucky/task/web/SpringBoot.java
@@ -0,0 +1,9 @@
+package com.lucky.task.web;
+
+/**
+ * @Author:chaoqiang.zhou
+ * @Description:
+ * @Date:Create in 17:05 2017/12/14
+ */
+public class SpringBoot {
+}
---
+++ spring-task/spring-task-web/src/main/java/com/lucky/task/web/controller/JobController.java
@@ -0,0 +1,12 @@
+package com.lucky.task.web.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @Author:chaoqiang.zhou
+ * @Description:
+ * @Date:Create in 17:06 2017/12/14
+ */
+@Controller
+public class JobController {
+}
