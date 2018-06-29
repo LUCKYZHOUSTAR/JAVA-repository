de76291c16da321e9303770b53219960feb1ff26
chaoqiang.zhou <1255963314@qq.com>
2018-06-29 15:40:03 +0800

    add Not

---
+++ groovy-lib/src/com/lucky/test/Hello.groovy
@@ -0,0 +1,17 @@
+package com.lucky.test
+/**
+ * @Author:chaoqiang.zhou
+ *
+ * @Date:Create in 下午3:27 2018/6/29
+ */
+
+
+class Hello {
+    static void main(String[] args) {
+        println("hello word")
+        println("hello word")
+
+        def x=5;
+        println(x)
+    }
+}
