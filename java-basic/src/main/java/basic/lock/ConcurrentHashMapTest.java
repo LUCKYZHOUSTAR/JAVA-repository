3924296d509412cf38f07b0fa14be8b9c00a3a6c
chaoqiang.zhou <1255963314@qq.com>
2018-10-09 11:20:52 +0800

    init

--- java-basic/src/main/java/basic/lock/ConcurrentHashMapTest.java
+++ java-basic/src/main/java/basic/lock/ConcurrentHashMapTest.java
@@ -1,5 +1,9 @@
 package basic.lock;
 
+import java.util.Arrays;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArrayList;
+
 /**
  * @Author:chaoqiang.zhou
  * @Description:HashTable容器在竞争激烈的并发环境下表现出效率低下的原因是所有访问HashTable的 线程都必须竞争同一把锁，假如容器里有多把锁，每一把锁用于锁容器其中一部分数据，那么
@@ -12,4 +16,22 @@ package basic.lock;
  * @Date:Create in 18:55 2017/11/28
  */
 public class ConcurrentHashMapTest {
+
+  public static void main(String[] args) {
+    int[] a = new int[2];
+    a[0] = 1;
+    //b和a共享一份区域
+    int[] b = a;
+    //此时a也会跟着变动，改变的是引用的对象的内存地址
+    b[1] = 5;
+    //这种方法是产生新的对象
+    int[] c = Arrays.copyOf(b, b.length);
+    c[1] = 9;
+    for(int i=0;i<a.length;i++){
+      System.out.println("a" + i + "=" + a[i]);
+      System.out.println("b" + i + "=" + b[i]);
+
+    }
+  }
+
 }
